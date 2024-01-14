d8ea75ecfe2cae76d788a4b5512cb8813068b87e
sanketnighot <sanketnighot25@gmail.com>
2024-01-14 13:04:24 +0530

    UI Fixes

--- pages/Collection.js
+++ pages/Collection.js
@@ -27,7 +27,7 @@ const Collection = () => {
                         onSubmit={(e) => { e.preventDefault(); setShowToken(true) }}>
                         <h3 className="text-center text-2xl md:text-4xl mb-2 text-[#26fb26]">Create your own SAT Token</h3>
                         <div className="flex-row justify-center md:flex text-center">
-                            <p className="md:text-left md:ml-4 text-sm md:text-xl mb-6 font-seven md:w-2/6 m-1">Define your collection name</p>
+                            <p className="md:text-left md:ml-4 text-sm md:text-xl md:mb-6 font-seven md:w-2/6 m-1">Define your collection name</p>
                             <input
                                 className="md:text-left md:mr-4 text-center text-sm md:text-xl font-seven mb-4 border-2 border-green-300 ring-2 ring-green-700 shadow-lg md:w-4/6 bg-transparent placeholder-green-300 w-4/6 md:w-full px-2"
                                 required
@@ -41,7 +41,7 @@ const Collection = () => {
                             />
                         </div>
                         <div className="flex-row justify-center md:flex text-center">
-                            <p className="md:text-left md:ml-4 text-sm md:text-xl font-seven mb-4 md:w-2/6 m-1">Enter token Admin address</p>
+                            <p className="md:text-left md:ml-4 text-sm md:text-xl font-seven md:mb-4 md:w-2/6 m-1">Enter token Admin address</p>
                             <input
                                 className="md:text-left md:mr-4 text-center text-sm  md:text-xl font-seven mb-4 border-2 border-green-300 ring-2 ring-green-700 shadow-lg md:w-4/6 bg-transparent placeholder-green-300 w-4/6 md:w-full px-2"
                                 placeholder="Enter Admin Address ..."
@@ -55,7 +55,7 @@ const Collection = () => {
                             />
                         </div>
                         <div className="flex-row justify-center md:flex text-center">
-                            <p className="md:text-left md:ml-4 text-sm md:text-xl font-seven mb-4 md:w-2/6 m-1">Describe your token in few words</p>
+                            <p className="md:text-left md:ml-4 text-sm md:text-xl font-seven md:mb-4 md:w-2/6 m-1">Describe your token in few words</p>
                             <textarea
                                 className="text-left md:mr-4 text-sm md:text-xl font-seven mb-4 h-40 border-2 border-green-300 ring-2 ring-green-700 shadow-lg bg-transparent placeholder-green-300 w-4/6 md:w-full p-2"
                                 placeholder="Describe Collection here ..."
