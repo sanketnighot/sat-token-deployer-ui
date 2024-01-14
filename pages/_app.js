0a7852f600cdf638a3ab321d9b9e2b1fcb5308c7
sanketnighot <sanketnighot25@gmail.com>
2024-01-14 20:58:45 +0530

    removed mask

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,14 @@ import React, { useState, useEffect } from 'react'
 import '@/styles/globals.css'
 import Footer from '@/components/Assets/Footer'
 import Header from '@/components/Assets/Header'
-import Mask from '@/components/Assets/Mask'
 
 export default function App({ Component, pageProps }) {
-    const [isAnimating, setIsAnimating] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setIsAnimating(false);
-        }, 5500); // Duration should match the CSS animation
-    });
-
 
     return (
         <>
-            {!isAnimating && <><Header />
-                <Component {...pageProps} />
-                <Footer /></>}
-            {isAnimating && <Mask />}
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
 
         </>
     )
