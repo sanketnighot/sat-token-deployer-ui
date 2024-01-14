00117879cfa2ba57c698b4db562d673a17776a56
sanketnighot <sanketnighot25@gmail.com>
2024-01-15 00:15:22 +0530

    Changed Loader background

--- components/Assets/Mask.js
+++ components/Assets/Mask.js
@@ -4,10 +4,10 @@ import Image from "next/image"
 export default function Mask() {
     return (
         <>
-            <div className="flex flex-wrap justify-center items-center min-h-screen">
+            <div className="flex flex-wrap justify-center items-center min-h-screen bg-[#131215]">
                 <div className="container min-h-24 w-4/6 p-8">
                     <Image
-                        className="cursor-pointer mx-auto"
+                        className="mx-auto"
                         src="/loader.webp"
                         width={600}
                         height={600}
