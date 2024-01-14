b48b7b9b64c2d8c5043c7cea7dc53dc66b58dc93
sanketnighot <sanketnighot25@gmail.com>
2024-01-14 21:09:34 +0530

    Added Loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react'
 import '@/styles/globals.css'
 import Footer from '@/components/Assets/Footer'
 import Header from '@/components/Assets/Header'
+import Mask from '@/components/Assets/Mask'
 
 export default function App({ Component, pageProps }) {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 5500);
+    });
 
     return (
         <>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
+            {!loading && <>
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+            </>}
+            {loading && <>
+                <Mask />
+            </>}
 
         </>
     )
