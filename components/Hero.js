fe4b39e08712f9d70f8d8ad130931a56ace9c080
sanketnighot <sanketnighot25@gmail.com>
2024-01-14 21:15:17 +0530

    Added Hero coin animation again

--- components/Hero.js
+++ components/Hero.js
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
 import Image from "next/image"
+import Loader from '../components/Assets/Loader';
 
 const Hero = () => {
+    const [isAnimating, setIsAnimating] = useState(true);
+    const [isClient, setIsClient] = useState(false); // New state to track if we're on the client
+    const router = useRouter();
+
+    useEffect(() => {
+        // This code runs only on the client
+        setIsClient(true);
+        setTimeout(() => {
+            setIsAnimating(false);
+        }, 100); // Duration should match the CSS animation
+    }, []);
+
+    const handleNavigate = () => {
+        setIsAnimating(true);
+        setTimeout(() => {
+            router.push('/Collection');
+        }, 500); // Duration should match the CSS animation
+    };
 
     return (
         <div className="min-h-screen flex items-center justify-center">
@@ -17,9 +36,21 @@ const Hero = () => {
                     width={175}
                     height={175}
                     alt="token image"
+                    onClick={handleNavigate}
                 />
                 <h2 className="text-md md:text-2xl mb-2">Click coin to create your own collection</h2>
             </div>
+            {isClient && isAnimating && (
+                <motion.div
+                    className="fixed top-0 left-0 z-10 w-full h-full bg-[#1b1b1b] circle-animation"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1.5 }}
+                >
+                    <div><Loader /></div>
+                </motion.div>
+            )}
         </div>
     )
 }
