369396e1aeefe54dfc863f0a05ae199e6aafe050
sanketnighot <sanketnighot25@gmail.com>
2024-01-31 15:32:05 +0530

    [update] mainnet changes and UI changes

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import Image from "next/image"
-
 const Hero = () => {
-    const router = useRouter();
-
-
-    const handleNavigate = () => {
-        router.push('/create');
-    };
 
     return (
         <div className="min-h-screen flex items-center justify-center">
@@ -16,14 +9,16 @@ const Hero = () => {
                 <h1 className="text-2xl md:text-6xl mb-2">Social Appreciation Tokens</h1>
                 <h2 className="text-lg md:text-4xl mb-2">{'<'} For the great Artists of Tezos {'>'}</h2>
                 <p className="text-md md:text-3xl font-seven mb-2">SATs stands for Social Appreciation Tokens, a revolutionary tokenization system on the Tezos blockchain that transforms the relationship between artists and their collectors. SAT's operate on the principle of decentralized promotion and ownership, creating a symbiotic ecosystem that benefits both artists and token holders.</p>
-                <Image
-                    className="cursor-pointer mx-auto"
-                    src="/coin.webp"
-                    width={175}
-                    height={175}
-                    alt="token image"
-                    onClick={handleNavigate}
-                />
+                <Link href="/create">
+                    <Image
+                        className="cursor-pointer mx-auto"
+                        src="/coin.webp"
+                        width={175}
+                        height={175}
+                        alt="token image"
+                    />
+                </Link>
+
                 <h2 className="text-md md:text-2xl mb-2">Click coin to create your own collection</h2>
             </div>
         </div>
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 export const NETWORK = 'mainnet';
 export const RPC = 'https://mainnet.ecadinfra.com';
-export const DEPLOYER_CONTRACT_ADDRESS = 'KT1KbPKoBwCs8pnqApTJTdmJX1kg56kmG4Cb';
+export const DEPLOYER_CONTRACT_ADDRESS = 'KT1VeGoaqrcvS88aHZ3wxTtE2AkipsSt8dnY';
 export const DECIMAL = 6;
 export const FEE = 5
