2e7d6149d36bd92910eefcbc72d93d99b60055c8
sanketnighot <sanketnighot25@gmail.com>
2024-02-24 18:21:47 +0530

    [update] Now contract deployment will be done directly

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 export const NETWORK = 'mainnet';
 export const RPC = 'https://mainnet.ecadinfra.com';
-export const DEPLOYER_CONTRACT_ADDRESS = 'KT1VeGoaqrcvS88aHZ3wxTtE2AkipsSt8dnY';
 export const DECIMAL = 6;
 export const FEE = 5
+export const FEE_RECIPIENT = 'tz1SAEgBew5tDdC2h37xGsR81V7YHopuxtvy';
--- utils/origination.js
+++ utils/origination.js
@@ -1,8 +1,10 @@
 import { dappClient } from './walletconnect';
-import { char2Bytes } from "@taquito/utils";
+import contractCode from './contract_code.json';
 import { MichelsonMap } from '@taquito/michelson-encoder';
-import { DEPLOYER_CONTRACT_ADDRESS, FEE, DECIMAL } from './config';
-import axios from 'axios';
+import { BigNumber } from 'bignumber.js';
+import { FEE, DECIMAL, FEE_RECIPIENT } from './config';
+
+BigNumber.config({ DECIMAL_PLACES: DECIMAL })
 
 
 export const deployContract = async (
@@ -18,15 +20,16 @@ export const deployContract = async (
     try {
         setIsLoading(true);
         setTxnMessage('Connecting Wallet ...');
-        await dappClient().connectAccount();
-        const metadata = MichelsonMap.fromLiteral({
+        await dappClient().CheckIfWalletConnected()
+        setTxnMessage('Preparing Metadata ...');
+        let metadata = MichelsonMap.fromLiteral({
             "": Buffer("tezos-storage:contents", "ascii").toString("hex"),
             contents: Buffer(
                 JSON.stringify({
                     version: "v0.0.1",
                     name: collectionData.collectionName,
                     description: collectionData.collectionDescription,
-                    authors: ["Deeployer < https://x.com/tzdeeployer >"],
+                    authors: ["SAT Token Deployer"],
                     source: {
                         tools: ["Smartpy"],
                     },
@@ -35,47 +38,53 @@ export const deployContract = async (
                 "ascii"
             ).toString("hex"),
         });
-        const tokenMetadata = {
-            symbol: collectionData.tokenSymbol,
-            name: collectionData.tokenName,
-            decimals: DECIMAL,
-            shouldPreferSymbol: "true",
-            description: collectionData.tokenDescription,
-            thumbnailUri: collectionData.tokenUrl
+        const tokenMetadata = MichelsonMap.fromLiteral({
+            0: {
+                token_id: 0,
+                token_info: MichelsonMap.fromLiteral({
+                    symbol: Buffer(collectionData.tokenSymbol, "ascii").toString("hex"),
+                    name: Buffer(collectionData.tokenName, "ascii").toString("hex"),
+                    decimals: Buffer(DECIMAL.toString(), "ascii").toString("hex"),
+                    shouldPreferSymbol: Buffer("true", "ascii").toString("hex"),
+                    description: Buffer(collectionData.tokenDescription, "ascii").toString("hex"),
+                    thumbnailUri: Buffer(collectionData.tokenUrl, "ascii").toString("hex"),
+                })
+            }
+        });
+        setTxnMessage('Setting up Storage ...');
+        let storage = {
+            administrator: collectionData.collectionAdmin,
+            last_token_id: 1,
+            ledger: MichelsonMap.fromLiteral({}),
+            metadata: metadata,
+            operators: {},
+            supply: (collectionData.tokenSupply * 1000000),
+            token_metadata: tokenMetadata,
         }
-        setTxnMessage('Uploading Metadata to IPFS...');
-        const res = await axios.post("/api/ipfs", { data: tokenMetadata })
-        console.log(res.data.hash, res.data.hash)
-        if (res.data.status === "error") {
-            return false
-        }
+        storage.ledger.set(collectionData.collectionAdmin, (collectionData.tokenSupply * 1000000))
         setTxnMessage('Waiting for you to sign Transaction ...');
         const tezos = await dappClient().tezos();
-        const transaction = await tezos.wallet
-            .at(DEPLOYER_CONTRACT_ADDRESS)
-            .then((contract) => {
-                return contract.methods.deploy_contract(
-                    collectionData.collectionAdmin,
-                    metadata,
-                    collectionData.tokenSupply * (10 ** DECIMAL),
-                    { "": char2Bytes(`ipfs://${res.data.hash}`) }
-                )
-                    .send({ amount: FEE * 1000000, mutez: true });
-            });
+        const batch = await tezos.wallet.batch()
+            .withTransfer({ to: FEE_RECIPIENT, amount: FEE })
+            .withOrigination({
+                code: contractCode,
+                storage: storage,
+            })
+            .send();
         setTxnMessage('Waiting for Confirmation ...');
-        await transaction.confirmation();
+        await batch.confirmation();
         await dappClient().disconnectWallet()
         setIsLoading(false);
         setTxnMessage();
         setShowSuccess(true);
         setSuccessMessage('Contract deployed successfully!');
-        setTransactionUrl(`https://tzkt.io/${transaction.opHash}`);
+        setTransactionUrl(`https://ghostnet.tzkt.io/${batch.opHash}`);
 
     } catch (error) {
         await dappClient().disconnectWallet()
         console.log(error);
         setIsLoading(false);
-        setShowError(true);
         setErrorMessage(error.message);
+        setShowError(true);
     }
 }
