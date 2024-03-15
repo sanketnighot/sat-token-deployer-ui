ff409afcae965bd15594a829f30e40f62a85a35a
sanketnighot <sanketnighot25@gmail.com>
2024-03-15 22:00:30 +0530

    [chore] Update height of container in airdrop page

--- pages/airdrop.js
+++ pages/airdrop.js
@@ -17,7 +17,7 @@ const airdrop = () => {
         <meta property="og:image" content={"/logo1.png"} />
         <meta property="og:url" content="https://deeployer.xyz" />
       </Head>
-      <div className="md:h-[50rem] flex items-center justify-center text-[#00ff00]">
+      <div className="md:h-[51rem] flex items-center justify-center text-[#00ff00]">
         <div className="container w-5/6 md:w-3/6">
           <div className="mb-4">
             <h1 className="text-center text-3xl md:text-5xl mb-2">
