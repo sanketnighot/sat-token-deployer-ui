f87aaba6b57fe65e308dfa82f35c531185c8860d
sanketnighot <sanketnighot25@gmail.com>
2024-03-30 23:01:17 +0530

    Update Tezos network configuration

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
-export const NETWORK = "mainnet"
-export const RPC = "https://mainnet.tezos.marigold.dev/"
-export const EXPLORER = "https://tzkt.io"
+export const NETWORK = "ghostnet"
+export const RPC = "https://ghostnet.tezos.marigold.dev/"
+export const EXPLORER = "https://ghostnet.tzkt.io"
 export const DECIMAL = 6
 export const FEE = 5
 export const FEE_RECIPIENT = "tz1SS3WhJiUs4YtkV1GMWNhCsxpUvNJcSFNw"
